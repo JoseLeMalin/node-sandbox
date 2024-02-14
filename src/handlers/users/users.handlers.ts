3a50f694246e6ca571da147489413b931d57c9cd
JoseLeMalin <seb_488@hotmail.fr>
2024-02-14 21:48:14 +0100

    Started users endpoints

---
+++ src/handlers/users/users.handlers.ts
@@ -0,0 +1,8 @@
+import { Request, Response } from "express";
+export const userLoginHandler = async (req: Request, res: Response) => {
+    
+    console.log(req);
+
+    console.log(res);
+
+};
--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import { Request, Response, NextFunction } from "express";
+import { userLoginHandler } from "../handlers/users/users.handlers";
 
 export const usersRouter = express.Router();
 
@@ -9,6 +10,20 @@ usersRouter.get(
   async (req: Request, res: Response, next: NextFunction) => {
     try {
       console.log("In the users get endpoint");
+
+      res.status(201);
+      res.send(`respond with a resource`);
+    } catch (error) {
+      next(error);
+    }
+  },
+);
+
+usersRouter.get(
+  "/login",
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      await userLoginHandler(req, res);
 
       res.status(201);
       res.send(`respond with a resource`);
