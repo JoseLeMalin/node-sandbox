fc85bd08bda4966ff5acd2cdfdc323ffde961ad5
Jose LeMalin <seb_488@hotmail.fr>
2024-02-21 18:14:26 +0100

    A commit a day

--- src/utils/crypto-module/hooks.ts
+++ src/utils/crypto-module/hooks.ts
@@ -4,7 +4,7 @@ import {
 } from "crypto";
 
 export const algorithm = "aes-192-cbc";
-// export const algorithm256 = "aes-256-cbc";
+export const algorithm256 = "aes-256-cbc";
 export const keyLength = 24; // Has to be 24 to match the aes-192-cbc algorithm
 export const password = "Password used to generate key";
 
