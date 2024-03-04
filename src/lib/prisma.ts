6900d299763f41188f62106e1989bd9018248ccf
JoseLeMalin <seb_488@hotmail.fr>
2024-03-04 22:05:55 +0100

    Updated prisma adapter with the ts fix proposed on github

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,6 +1,6 @@
 // import { PrismaClient } from "@prisma/client/edge";
 import { PrismaClient } from "@prisma/client";
-// import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaPg } from "@prisma/adapter-pg";
 import { Pool } from "pg";
 
 /**
@@ -14,10 +14,10 @@ import { Pool } from "pg";
 
 const connectionString = `${process.env.DATABASE_URL}`;
 
-// const pool = new Pool({ connectionString })
-// const adapter = new PrismaPg(pool)
-// export const prisma = new PrismaClient({ adapter })
-export const prisma = new PrismaClient({
-  datasourceUrl: connectionString,
-});
+const pool = new Pool({ connectionString })
+const adapter = new PrismaPg(pool)
+export const prisma = new PrismaClient({ adapter })
+// export const prisma = new PrismaClient({
+//   datasourceUrl: connectionString,
+// });
 // use `prisma` in your application to read and write data in your DB
